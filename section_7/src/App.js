eae78d652d33fa0355d1789251608427ff3bc2fc
Renan Boaventura <renandev@outlook.com>
2023-10-14 19:00:12 -0300

    fixed project setup

---
+++ section_7/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from './components/Button'
+
+function App(){
+    return (
+    <div>
+        <h2>App</h2>       
+        <Button />
+    </div>
+)}
+
+export default App
