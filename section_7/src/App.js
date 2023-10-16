9a83299752217071432ef7236bd0e5f86d557eca
Renan Boaventura <renandev@outlook.com>
2023-10-16 12:24:15 -0300

    implement an code example using the old prop-types library

--- section_7/src/App.js
+++ section_7/src/App.js
@@ -5,8 +5,22 @@ import Button from './components/Button'
 function App(){
     return (
     <div>
-        <h2>App</h2>       
-        <Button />
+        <h2>App</h2>
+        <div>
+        <Button primary>Click me</Button>
+        </div>    
+        <div>
+        <Button success>Buy now</Button>
+        </div>
+        <div>
+        <Button warning>See deal</Button>
+        </div>
+        <div>
+        <Button danger>Hide Ads</Button>
+        </div>
+        <div>
+        <Button secondary>Something</Button>
+        </div>
     </div>
 )}
 
--- section_7/src/components/Button.js
+++ section_7/src/components/Button.js
@@ -1,9 +1,49 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-function Button() {
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded
+}) {
   return (
-    <Button>Button</Button>
+    <button>{children}</button>
   )
+}
+
+Button.propTypes = {
+  checkVariationValue: ({
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    }) => {
+   const count = Number(!!primary)
+   + Number(!!secondary) 
+   + Number(!!success)
+   + Number(!!warning)
+   + Number(!!danger)
+
+   if (count > 1){
+    return new Error("You should select one of the following options: primary, secondary, success, warning or danger. Can't choose two or more at once.")
+   } else if (count < 1) {
+    return new Error("You should select one of the following options: primary, secondary, success, warning or danger.")
+   }
+  },
+  children: PropTypes.any,
+  primary: PropTypes.bool,
+  secondar: PropTypes.bool,
+  success: PropTypes.bool,
+  warning: PropTypes.bool,
+  danger: PropTypes.bool,
+  outline: PropTypes.bool,
+  rounded: PropTypes.bool
 }
 
 export default Button
