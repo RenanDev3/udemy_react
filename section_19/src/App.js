50cf59ff8aaeb5629fa2fe9eb679bf1bb057c7aa
Renan Boaventura <renandev@outlook.com>
2023-10-27 10:45:28 -0300

    implement boilerplate for App component

--- section_19/src/App.js
+++ section_19/src/App.js
@@ -1,7 +1,24 @@
 import React from "react";
 
+import MoviePlaylist from "./components/MoviePlaylist"
+import SongPlaylist from "./components/SongPlaylist"
+
 function App(){
-    return <div>App</div>
+    function handleResetClick(){
+
+    }
+
+    return (
+        <div className="container is-fluid">
+        <button onClick={() => handleResetClick()} className="button is-danger">
+          Reset Both Playlists
+        </button>
+        <hr />
+        <MoviePlaylist />
+        <hr />
+        <SongPlaylist />
+      </div>   
+    )
 }
 
 export default App
