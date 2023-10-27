b17a94725b024cc760e930ab258438ec5e7ca575
Renan Boaventura <renandev@outlook.com>
2023-10-27 10:49:15 -0300

    implement style with css file and bulma framework

--- section_19/src/index.js
+++ section_19/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './style.css'
+import "bulma/css/bulma.css"
 import App from './App'
 
 const el = document.getElementById('root')
