2ab0313a71539611dc0205559a1807f8e553006b
Renan Boaventura <renandev@outlook.com>
2023-11-02 12:29:45 -0300

    implement the component to sum the value of the cars

--- section_20/src/components/CarValue.js
+++ section_20/src/components/CarValue.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 export default function CarValue() {
+
+  const totalCost = useSelector(({cars: {data, searchTerm}}) => {
+    return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase())).reduce((acc, curr) => acc + curr.cost, 0)
+  })
+
   return (
-    <div>CarValue</div>
+    <div className='car-value'>
+      Total cost:  ${totalCost}
+    </div>
   )
 }
