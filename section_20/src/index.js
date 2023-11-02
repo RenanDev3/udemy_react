919d4ea7779492f8271f847d66e64319fc504bb7
Renan Boaventura <renandev@outlook.com>
2023-11-02 12:13:57 -0300

    implement style

--- section_20/src/index.js
+++ section_20/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
+import 'bulma/css/bulma.css'
 
 import App from './App'
 import { store } from './store'
+
+
 import './styles.css'
 
 const el = document.getElementById("root")
