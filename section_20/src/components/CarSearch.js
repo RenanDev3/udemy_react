112cdd1833dcedda219ffe2bf70ef9638c9b49d2
Renan Boaventura <renandev@outlook.com>
2023-11-02 12:29:25 -0300

    implement the search component

--- section_20/src/components/CarSearch.js
+++ section_20/src/components/CarSearch.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+
+import { changeSearchTerm } from '../store'
 
 function CarSearch() {
+  const { searchTerm } = useSelector((state) => state.cars)
+  const dispatch = useDispatch()
+
+  function handleChangeSearchTerm(event){
+    dispatch(changeSearchTerm(event.target.value))
+  }
+
   return (
-    <div>CarSearch</div>
+    <div className='list-header'>
+      <h3 className='title is-3'>My cars</h3>
+      <div className='search field is-horizontal'>
+        <label className='label'>Search</label>
+        <input 
+        className='input'
+        value={searchTerm}
+        onChange={handleChangeSearchTerm}
+        />
+      </div>
+
+    </div>
   )
 }
 
