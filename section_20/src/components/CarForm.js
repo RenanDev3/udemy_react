79992b0fdca730404da6bcf75b97fcd465e2a986
Renan Boaventura <renandev@outlook.com>
2023-11-02 12:28:47 -0300

    implement the form to insert new cars

--- section_20/src/components/CarForm.js
+++ section_20/src/components/CarForm.js
@@ -1,8 +1,61 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { changeName, changeCost, addCar } from '../store'
 
 function CarForm() {
+  const dispatch = useDispatch()
+  const { carName, carCost } = useSelector((state) => {
+    return {
+      carName: state.form.name,
+      carCost: state.form.cost
+    }
+  })
+
+  function handleChangeName(event){
+    dispatch(changeName(event.target.value))
+  }
+
+  function handleChangeCost(event){
+    const targetCost = parseInt(event.target.value) || 0
+    dispatch(changeCost(targetCost))
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+
+    dispatch(addCar({
+      name: carName,
+      cost: carCost
+    }))
+  }
+
   return (
-    <div>CarForm</div>
+    <div className='car-form panel'>
+      <h4 className='subtitle is-3'>Add car</h4>
+      <form onSubmit={handleSubmit}>
+        <div className='field-group'>
+          <div className='field'>
+            <label className='label'>Name</label>
+            <input 
+            className='input is-expanded'
+            value={carName}
+            onChange={handleChangeName}
+            />
+            <label className='label'>Cost</label>
+            <input 
+            className='input is-expanded'
+            type='number'
+            value={carCost}
+            onChange={handleChangeCost}
+            />
+          </div>
+        </div>
+        <div className='field'>
+          <button className='button is-link'>Submit</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
