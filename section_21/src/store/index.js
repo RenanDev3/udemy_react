8b240c348afb072157e732f004e355f7519a3487
Renan Boaventura <renandev@outlook.com>
2023-11-05 09:46:35 -0300

    create the redux store

--- section_21/src/store/index.js
+++ section_21/src/store/index.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import { usersReducer } from "./usersSlice";
+import { usersReducer } from "./slices/usersSlice";
 
 const store = configureStore({
     reducer: {
