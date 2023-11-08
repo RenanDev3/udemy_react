8db9def4fb83d4f6a6ec72ae7a1cdc60210246fe
Renan Boaventura <renandev@outlook.com>
2023-11-08 10:13:03 -0300

    refator user list item to implement the expandable panel

--- section_21/src/components/UserListItem.js
+++ section_21/src/components/UserListItem.js
@@ -4,6 +4,8 @@ import { TiDelete } from 'react-icons/ti'
 import useThunk from '../hooks/use-thunk'
 import { deleteUser } from '../store'
 import Button from './Button'
+import ExpandablePanel from './ExpandablePanel'
+import AlbumsList from './AlbumsList'
 
 function UserListItem({user}) {
     const [ doDeleteUser, isDeletingUser, deletingUserError ] = useThunk(deleteUser)
@@ -12,18 +14,18 @@ function UserListItem({user}) {
         doDeleteUser(userId)
       }
 
-    return (
-        <div className='mb-2 border rounded'>
-            <div className='flex p-2 justify-between items-center cursor-pointer'>
-                <div className='flex flex-row items-center justify-between'>
+    const header = <>
                     <Button className='mr-3' loading={isDeletingUser} onClick={() => handleDeleteUser(user.id)}>
                         <TiDelete />
                     </Button>
                     { deletingUserError && <div>Error at deleting user</div> }
                     {user.name}
-                </div>
-            </div>
-        </div>
+                    </>
+
+    return (
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user}/>
+        </ExpandablePanel>
     )
 }
 
