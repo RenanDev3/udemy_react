02237b3bff59341eb0f646a2ab6f30de6d21f2d0
Renan Boaventura <renandev@outlook.com>
2023-11-08 07:48:33 -0300

    code refactor to implement custom hook use-thunk

--- section_21/src/components/UsersList.js
+++ section_21/src/components/UsersList.js
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react'
-import  {useDispatch, useSelector } from 'react-redux'
+import  {useSelector } from 'react-redux'
 
+import useThunk from '../hooks/use-thunk'
 import { fetchUsers } from '../store'
 import { addUser } from '../store'
 import Skeleton from './Skeleton'
 import Button from './Button'
 
 function UsersList() {
-  const dispatch = useDispatch()
-  const {isLoading, data, error} = useSelector((state) => state.users)
+  const [doFetchUsers, isLoadingUsers, loadingUsersErrors] = useThunk(fetchUsers)
+  const [doAddUser, isCreatingUser, creatingUserError] = useThunk(addUser)
+  const { data } = useSelector((state) => state.users)
 
   useEffect(() => {
-    dispatch(fetchUsers())
-  }, [dispatch])
+    doFetchUsers()
+  }, [doFetchUsers])
 
-  if (isLoading){
+  if (isLoadingUsers){
     return <h1><Skeleton times={6} customClasses="h-10 w-full"/></h1>
   }
-  if (error){
-    return <h1>Error: {error.message}</h1>
+  if (loadingUsersErrors){
+    return <h1>Error: {loadingUsersErrors.message}</h1>
   }
 
   const renderedUsers = data.map((user) => {
@@ -26,16 +28,18 @@ function UsersList() {
   })
 
   function handleAddUser(){
-    dispatch(addUser())
+    doAddUser()
   }
 
   return (
     <div>
       <div className='flex flex-row justify-between m-3'>
         <h1 className='m-2 text-xl'>Users</h1>
-        <Button primary onClick={handleAddUser}>
-          + Add User
-        </Button>
+        {
+          isCreatingUser ? "Creating user..." :
+          <Button primary onClick={handleAddUser}>+ Add User</Button>
+        }
+        { creatingUserError && "Error at creating new user." }
       </div>
       <div>{renderedUsers}</div>
     </div>
